6af3f7ab6fe1d7ea4a712b78ab467b779ab4237a
Gabriel Vidal <gabrieldkvl@icloud.com>
2023-06-27 18:57:59 +0200

    Learning process

--- Lesson 2/index.php
+++ Lesson 2/index.php
@@ -10,10 +10,10 @@
    <h1>This text is written in HTML</h1>
    
    <?php 
-   #This is a comment in PHP
-   #Using semicolon to end a statement
+   /*This is a multi line comment in PHP
+   Using semicolon to end a statement*/
    echo "This text is wtitten in PHP";
-   #The closing tag implies a semicolon, and isn't needed for the last line in a block of PHP
+   //The closing tag implies a semicolon, and isn't needed for the last line in a block of PHP
    echo "The last line in PHP code doesn't need a semicolon"
    ?>
 
